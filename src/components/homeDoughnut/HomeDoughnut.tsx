68b34df1f6289f14b3fe20a45e7609de821ac9ee
IlalovX <ilalovx@gmail.com>
2025-03-14 03:46:43 +0500

    fix

--- src/components/homeDoughnut/HomeDoughnut.tsx
+++ src/components/homeDoughnut/HomeDoughnut.tsx
@@ -5,8 +5,8 @@ function HomeDoughnut() {
 	const isDarkMode = theme.palette.mode === 'dark'
 	return (
 		<div className='flex items-center justify-center'>
-			<div className='relative w-40 h-40 flex items-center justify-center'>
-				<div className='absolute inset-0 bg-gradient-to-r from-green-300 to-green-600 rounded-full p-5'>
+			<div className='relative w-30 h-30 flex items-center justify-center'>
+				<div className='absolute inset-0 bg-gradient-to-r from-green-300 to-green-600 rounded-full p-4'>
 					<div
 						className='w-full h-full rounded-full flex flex-col items-center justify-center'
 						style={{
